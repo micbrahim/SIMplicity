1ea58c510cd5943d72939b1b2e734a96012c635b
micibr <micibr@cs.washington.edu>
2023-10-14 17:07:32 -0700

    Different call functionalities

--- src/main.js
+++ src/main.js
@@ -2,9 +2,29 @@ import { initWebRTC } from './webrtc.js';
 
 const startCallBtn = document.getElementById('startCallBtn');
 const callSection = document.getElementById('callSection');
+const holdCallBtn = document.getElementById('holdCallBtn');
+const resumeCallBtn = document.getElementById('resumeCallBtn');
+const endCallBtn = document.getElementById('endCallBtn');
 
 startCallBtn.addEventListener('click', () => {
     startCallBtn.disabled = true;  // Disable the button to prevent multiple clicks
     callSection.classList.remove('hidden');  // Display the call section
     initWebRTC();
-});
+})
+
+function makeOutboundCall(){
+
+}
+
+holdCallBtn.addEventListener('click', () => {
+
+})
+
+resumeCallBtn.addEventListener('click', () => {
+
+} )
+
+endCallBtn.addEventListener('click', () => {
+    
+})
+;
